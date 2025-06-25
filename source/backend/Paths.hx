e00853751d53ffe4ad7abb22e9b4ea401749de16
Homura <egzozu.be1.bas@gmail.com>
2025-06-25 19:25:58 +0300

    this shouldn't be Sound.fromFile only

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -454,17 +454,21 @@ class Paths
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
 		try {
-			if(!currentTrackedSounds.exists(gottenPath))
-			#if MODS_ALLOWED
-				currentTrackedSounds.set(gottenPath, Sound.fromFile(#if !mobile './' + #end gottenPath));
-			#else
-			{
-				var folder:String = '';
-				if(path == 'songs') folder = 'songs:';
-		
-				currentTrackedSounds.set(gottenPath, OpenFlAssets.getSound(folder + getPath('$path/$key.$SOUND_EXT', SOUND, library)));
+			if (!currentTrackedSounds.exists(gottenPath)) {
+				var sound:Sound = null;
+				final fullPath:String = #if !mobile './' + #end gottenPath;
+
+				if (sys.FileSystem.exists(fullPath))
+					sound = Sound.fromFile(fullPath);
+				else
+				{
+					var folder:String = '';
+					if(path == 'songs') folder = 'songs:';
+					sound = OpenFlAssets.getSound(folder + getPath('$path/$key.$SOUND_EXT', SOUND, library));
+				}
+
+				currentTrackedSounds.set(gottenPath, sound);
 			}
-			#end
 		} catch (e:Dynamic) {
 			if (ClientPrefs.isDebug())
 				Sys.println('Paths.returnSound(): SOUND NOT FOUND: $key');
